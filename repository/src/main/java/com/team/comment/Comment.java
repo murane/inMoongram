750b16a66dc854f5c8b7e88a542e589cfa6a2b27
Junsu <murane@naver.com>
2021-07-22 13:58:39 +0900

    feat: Comment 엔티티 연관관계 메소드 설정 및 수정

--- repository/src/main/java/com/team/comment/Comment.java
+++ repository/src/main/java/com/team/comment/Comment.java
@@ -1,15 +1,22 @@
 package com.team.comment;
 
 import com.team.post.Post;
+import com.team.tag.CommentTaggedKeyword;
+import com.team.tag.CommentTaggedUser;
 import com.team.user.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -21,22 +28,56 @@ public class Comment {
 
     private String content;
 
-    @CreatedDate
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id")
+    @JoinColumn(name = "post_id", nullable = false)
     private Post post;
 
+    @JoinColumn(name = "super_comment_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Comment superComment;
+
+    @OneToMany(mappedBy = "superComment", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<Comment> subComments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "comment", orphanRemoval = true, cascade = CascadeType.ALL)
+    private Set<CommentTaggedKeyword> commentTaggedKeywords = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "comment", orphanRemoval = true, cascade = CascadeType.ALL)
+    private Set<CommentTaggedUser> commentTaggedUsers = new LinkedHashSet<>();
+
     @Builder
-    public Comment(String content, User user, Post post) {
+    public Comment(Long id, String content, User user, Post post, Comment superComment) {
+        this.id = id;
         this.content = content;
         this.user = user;
         this.post = post;
+        this.superComment = superComment;
+        if (superComment != null) {
+            superComment.addSubComment(this);
+        }
         post.addComment(this);
+    }
+
+    public void addAllTaggedKeywords(List<CommentTaggedKeyword> commentTaggedKeywords) {
+        this.commentTaggedKeywords.addAll(commentTaggedKeywords);
+    }
+
+    public void addAllTaggedUsers(List<CommentTaggedUser> commentTaggedUsers) {
+        this.commentTaggedUsers.addAll(commentTaggedUsers);
+    }
+
+    public void addSubComment(Comment comment) {
+        subComments.add(comment);
+    }
+
+    public void deleteSubComment(Comment comment) {
+        subComments.remove(comment);
     }
 }
